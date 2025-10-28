93e9073cf95828edd533ecd0a6b00d56a79400a6
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-10-28 16:10:57 +0530

    Update Project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ias from "../pages/img/ias.png";
 import unsplash from "../pages/img/unsplash.png";
 import cocktail from "../pages/img/cocktail.png";
-import jobify from "../pages/img/payrolldashboard.png
-";
+// import jobify from "../pages/img/payrolldashboard.png
+// ";
 
 import profile from "../pages/img/profile.png";
 import { GlobalContext } from "../components/ContextApi";
