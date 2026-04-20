ee789241467c188d098d46869f84fa27ea0454f2
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2026-04-20 18:29:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
         <Route path="/github" element={<Github />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
-      <div className={`w-20 h-10 leading-10 m-auto fixed top-60 right-0 shadow-xl  shadow-slate-500 ${isDarkTheme?'bg-black ':'bg-pink-200 '} ${show && <Feedback/>}  text-center rounded-2xl  cursor-pointer translate-x-12 hover:translate-x-0 transition-all 6s`}>
+      <div className={`w-20 h-10 leading-10 m-auto fixed top-80 right-0 shadow-xl  shadow-slate-500 ${isDarkTheme?'bg-black ':'bg-pink-200 '} ${show && <Feedback/>}  text-center rounded-2xl  cursor-pointer translate-x-12 hover:translate-x-0 transition-all 6s`}>
       <Link to='/feedback'><p>feedback</p></Link>
       </div>
     </div>
