ba99c9a927ff4205c4730c59b4a8aa67ac351e2e
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2026-04-20 19:06:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Github from "./components/Github";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Feedback from "./pages/Feedback";
+
 import { GlobalContext } from "./components/ContextApi";
 import { useState } from "react";
 
@@ -29,9 +29,7 @@ const App = () => {
         <Route path="/github" element={<Github />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
-      <div className={`w-20 h-10 leading-10 m-auto fixed top-80 right-0 shadow-xl  shadow-slate-500 ${isDarkTheme?'bg-black ':'bg-pink-200 '} ${show && <Feedback/>}  text-center rounded-2xl  cursor-pointer translate-x-12 hover:translate-x-0 transition-all 6s`}>
-      <Link to='/feedback'><p>feedback</p></Link>
-      </div>
+     
     </div>
   );
 };
